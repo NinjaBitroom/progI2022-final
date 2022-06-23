069074cb7cb7e067cf4ec10ead690593a8cb47d3
NinjaBitroom <gabrielbitercourt@gmail.com>
2022-06-23 18:18:10 -0400

    Anime todos os botões da "nav_bar"

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
-botao_nav_a = document.querySelector('nav ul li a')
+botao_nav_a = document.querySelectorAll('nav ul li a')
 
-botao_nav_a.onclick = function() {
-    bg_antiga = botao_nav_a.style.backgroundColor
-    cor_antiga = botao_nav_a.style.color
-
-    botao_nav_a.style.backgroundColor = 'black'
-    botao_nav_a.style.color = 'white'
-
-    setTimeout(function() {
-    botao_nav_a.style.backgroundColor = bg_antiga
-    botao_nav_a.style.color = cor_antiga
-    }, 1000)
+for (let i = 0; i < botao_nav_a.length; i++) {
+    const botao_nav = botao_nav_a[i];
+    botao_nav.onclick = function() {
+        bg_antiga = botao_nav.style.backgroundColor
+        cor_antiga = botao_nav.style.color
+    
+        botao_nav.style.backgroundColor = 'black'
+        botao_nav.style.color = 'white'
+    
+        setTimeout(function() {
+        botao_nav.style.backgroundColor = bg_antiga
+        botao_nav.style.color = cor_antiga
+        }, 1000)
+    }
 }
