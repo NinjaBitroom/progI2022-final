4110e28535f9d0c7788117de8d96723dd83ac309
NinjaBitroom <gabrielbitercourt@gmail.com>
2022-06-23 22:06:54 -0400

    Finalize a integração da tabuada

--- tasks/tabuada/script.js
+++ tasks/tabuada/script.js
@@ -1,7 +1,14 @@
-numero = parseFloat(prompt('Digite um número:'))
-for (let i = 0; i <= 10; i++) {
-    document.getElementById('adicao').innerHTML += '<p>'+numero+' + '+i+' = '+(numero+i)+'</p>'
-    document.getElementById('subtracao').innerHTML += '<p>'+numero+' - '+i+' = '+(numero-i)+'</p>'
-    document.getElementById('multiplicacao').innerHTML += '<p>'+numero+' * '+i+' = '+(numero*i)+'</p>'
-    document.getElementById('divisao').innerHTML += '<p>'+numero+' / '+i+' = '+(numero/i)+'</p>'
+iniciar.onclick = function() {
+    numero = parseFloat(prompt('Digite um número:'))
+    paragrafos = document.querySelectorAll('.card div p')
+    for (let i = 0; i < paragrafos.length; i++) {
+        const paragrafo = paragrafos[i];
+        paragrafo.remove()
+    }
+    for (let i = 0; i <= 10; i++) {
+        document.getElementById('adicao').innerHTML += '<p>'+numero+' + '+i+' = '+(numero+i)+'</p>'
+        document.getElementById('subtracao').innerHTML += '<p>'+numero+' - '+i+' = '+(numero-i)+'</p>'
+        document.getElementById('multiplicacao').innerHTML += '<p>'+numero+' * '+i+' = '+(numero*i)+'</p>'
+        document.getElementById('divisao').innerHTML += '<p>'+numero+' / '+i+' = '+(numero/i)+'</p>'
+    }
 }
